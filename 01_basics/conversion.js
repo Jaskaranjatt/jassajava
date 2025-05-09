f4d7b79e10c7a41add560f5e304d5373990576df
Jaskaranjatt <323jaskaransingh@gmail.com>
2025-05-09 11:11:33 +0000

    container config done

---
+++ 01_basics/conversion.js
@@ -0,0 +1,21 @@
+let score=89
+let score2=String(score)
+console.log(typeof(score2));// prints string
+let boolscore=Boolean(score)
+console.log(boolscore);//prints true
+let str=""
+let numstr=Number(str)
+console.log(numstr);//prints 0
+let str2=null
+console.log(str2);//prints null
+let str3=String(str2)
+console.log(str3);//prints null
+let str4="45fg"
+console.log(Number(str4));//prints NaN not a number
+console.log(Number(null)); //prints 0
+console.log(Number(undefined));//prints NaN
+
+
+
+
+
---
+++ 01_basics/conversion2.js
@@ -0,0 +1,6 @@
+console.log(1+"2");//prints 12
+console.log("2"+1);//prints 21
+console.log(1+2+"2");//prints 32
+console.log("Jaskaran"+" Jatt");//prints Jaskaran Jatt
+//write only the code which is easy to read for other user
+console.log(1+"2"+1);//prints 121
