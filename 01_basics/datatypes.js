9c34681d8752ea9d50f1a13a817c0989ef8b136e
Jaskaranjatt <323jaskaransingh@gmail.com>
2025-05-09 10:17:26 +0000

    container config done

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,12 @@
+"use strict" //use only new verson of javascript but we can also access by not writing it 
+// alert(3+3) we are using nodejs not browser so working
+let accouuntName="Jaskaran"// its a string
+let accountNumber=5//its number its range 0f 2^53
+let accountExist=true//its boolean
+//symbol used when we will use react
+// undefined when  no value is asiigned;
+//null its a selfalone value when there is no value
+// object
+//typeof is used to check datatype
+console.log(typeof null) // gives object
+console.log(typeof undefined)// gives undefined because undefined is datatype
