e45440287ef8fff45efc7caa1611aed8d396faa1
Jaskaranjatt <323jaskaransingh@gmail.com>
2025-05-09 06:51:02 +0000

    container config done

---
+++ 01_basics/variable.js
@@ -0,0 +1,8 @@
+const accountId=1234
+var accountName="Jaskaran"
+let accountCity="Jalandhar"
+var accountState;
+// if const is used we cant change value of variable but in var and let we can
+// var and let are almost same but not use var because of its scope.
+console.log(accountId)
+console.table([accountCity,accountId,accountName,accountState])
