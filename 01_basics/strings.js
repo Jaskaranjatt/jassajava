ca4df5bcd2312e3136732c0c5b4a9083a23f6510
Jaskaranjatt <323jaskaransingh@gmail.com>
2025-05-10 07:41:12 +0000

    container config done

---
+++ 01_basics/strings.js
@@ -0,0 +1,23 @@
+// strings can be declared in diiferent methods
+const name="Jaskaran"
+//2 way
+const age=new String("19")
+const newName="Jaskaran Singh Rakhra"
+const spaceName="    Jaskaran     "
+//we know that we can concat using + but ther is different way also
+// console.log(name+age);
+// console.log(`My name is ${name} my age is ${age}`);// same as formated string in python
+//there any many diiferent methods
+// console.log(name.__proto__);// shows the empty object
+// console.log(name.length);//prints length of string
+// console.log(name.charAt(3));//returns k
+// console.log(name.bold());// used in html
+// console.log(name.substr(0,4));//prints from 0 to 3
+// console.log(newName.split(" "));
+//console.log(name.toUpperCase());
+// console.log(spaceName.trim());//removes spaces from start and back
+// console.log(spaceName.trimStart());// removes space from start
+// console.log(newName.replace('k','a'));//replaces k to a
+// console.log(newName.at(3));//gives the char wrt index 3
+// console.log(newName.slice(1,4));//slices the string negative index can also be used
+// console.log(newName.lastIndexOf('a'));//last index of given character
